7dea1bff598f756d9893d1326f07630971cfc597
John Rose <john.rose3@comcast.net>
2023-02-08 10:31:11 -0800

    Update gatsby-config.js

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,1 +1,22 @@
-
+export default {
+  siteMetadata: {
+    title: `Slicks Slices`,
+    siteUrl: 'https://gatsby.pizza',
+    description: 'The best pizza place in Hamilton!',
+    twitter: '@slicksslices',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
+    // this is the name of the plugin we are adding
+    {
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: 'wgecvc8p',
+        dataset: 'production',
+        watchMode: true,
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+  ],
+};
