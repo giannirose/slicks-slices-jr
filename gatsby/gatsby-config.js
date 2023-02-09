12484c8c4c0b0f2e3e12116984787cd92bc20ae1
John Rose <john.rose3@comcast.net>
2023-02-09 10:41:43 -0800

    update from finished files
      pathPrefix: '/pizza',Previous version did not have line:

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,1 +1,23 @@
-
+export default {
+  pathPrefix: '/pizza',
+  siteMetadata: {
+    title: `Slicks Slices`,
+    siteUrl: 'https://gatsby.pizza',
+    description: 'The best pizza place in Hamilton!',
+    twitter: '@slicksslices',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
+    // this is the name of the plugin we are adding
+    {
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: 'wgecvc8p',
+        dataset: 'production',
+        watchMode: true,
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+  ],
+};
