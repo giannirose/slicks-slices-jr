276abc2b4a05591b887fae016c16c06d3eff11c7
John Rose <john.rose3@comcast.net>
2023-02-13 09:45:36 -0800

    add media query to PaginationStyles

--- gatsby/src/components/pagination.js
+++ gatsby/src/components/pagination.js
@@ -25,6 +25,12 @@ const PaginationStyles = styled.div`
       pointer-events: none;
       color: var(--grey);
     }
+    @media (max-width: 800px) {
+      .word {
+        display: none;
+      }
+      font-size: 1.4rem;
+    }
   }
 `;
 
