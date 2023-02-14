40225a0bafe51b2677b50cf0e0c2ca9def240dca
John Rose <john.rose3@comcast.net>
2023-02-13 22:46:32 -0800

    add className logo-item to logo

--- gatsby/src/components/Nav.js
+++ gatsby/src/components/Nav.js
@@ -86,7 +86,7 @@ export default function Nav() {
         <li>
           <Link to="/pizzas">Pizza Menu</Link>
         </li>
-        <li>
+        <li className="logo-item">
           <Link to="/">
             <Logo />
           </Link>
